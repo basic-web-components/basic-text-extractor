dbae04cbc372b378dbe776bb50e5ee5b0ebb630f
Jan Miksovsky <jan@quickui.org>
2015-06-09 16:31:50 -0700

    Pick up some newly-ported components

--- test/basic.tests.js
+++ test/basic.tests.js
@@ -10,7 +10,7 @@ suite('basic', function() {
 
   test('default value', function(done) {
     var fixture = document.createElement('basic-text-extractor');
-    container.appendChild(fixture);
+    Polymer.dom(container).appendChild(fixture);
     flush(function() {
       assert.equal(fixture.value, undefined);
       done();
@@ -19,11 +19,11 @@ suite('basic', function() {
 
   test('changed value', function(done) {
     var fixture = document.createElement('basic-text-extractor');
-    container.appendChild(fixture);
+    Polymer.dom(container).appendChild(fixture);
     flush(function() {
       assert.equal(fixture.value, undefined);
       var content = 'foo';
-      fixture.innerHTML = content;
+      Polymer.dom(fixture).innerHTML = content;
       flush(function() {
         assert.equal(fixture.value, content);
         done();
@@ -32,4 +32,3 @@ suite('basic', function() {
   });
 
 });
-
